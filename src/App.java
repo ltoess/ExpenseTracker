8e57e184731dc0859e52beeaae74e7dbc2c2c55b
Luke Oess <lukeoess@gmail.com>
2025-08-26 16:12:10 -0500

    fixed up syntax error in filemanager and began app class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,30 @@
+
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Scanner;
+
 public class App {
 
-    // main/ui layer 
+    private static final Path DEFAULT_FILE = Path.of("expenses.tsv");
+
+    public static void main(String[] args) {
+        ExpenseManager mgr = new ExpenseManager();
+        FileManager fm = new FileManager();
+        Scanner scan = new Scanner(System.in); 
+
+        try {
+            List<Expense> loaded = fm.read(DEFAULT_FILE);
+            mgr.replaceAll(loaded);
+            
+            if(loaded.isEmpty())
+                System.out.println("Loaded " + loaded.size() + " expenses from " + DEFAULT_FILE);
+
+        } catch (Exception e) {
+            System.out.println("Couldn't load existing file " + "(" + e.getMessage() + ")");
+        }
+
+        // begin gui logic
+
+    }
 
 }
--- src/FileManager.java
+++ src/FileManager.java
@@ -1,9 +1,11 @@
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileManager {
@@ -29,7 +31,7 @@ public class FileManager {
         if(!Files.exists(path))
             return list; 
 
-        try(BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_*)){
+        try(BufferedReader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8)){
             String line; 
             boolean first = true;
             while((line = reader.readLine()) != null) {
