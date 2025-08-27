c7311e6c9c6cb3a017c94dc36ae4595dea2cf269
ltoess <lukeoess@gmail.com>
2025-08-27 16:14:46 -0500

    printmenu method

--- src/App.java
+++ src/App.java
@@ -12,6 +12,7 @@ public class App {
         FileManager fm = new FileManager();
         Scanner scan = new Scanner(System.in); 
 
+        // try to auto load on startup
         try {
             List<Expense> loaded = fm.read(DEFAULT_FILE);
             mgr.replaceAll(loaded);
@@ -23,8 +24,27 @@ public class App {
             System.out.println("Couldn't load existing file " + "(" + e.getMessage() + ")");
         }
 
-        // begin gui logic
+        while(true) {
+            // printmenu method 
+        }
+
+        
 
     }
 
+
+    
+    private static void printMenu() {
+        System.out.println();
+        System.out.println("==== Expense Tracker ====");
+        System.out.println("1) Add expense");
+        System.out.println("2) List expenses");
+        System.out.println("3) Remove expense");
+        System.out.println("4) Total by category");
+        System.out.println("5) Monthly total");
+        System.out.println("6) Save to file");
+        System.out.println("7) Load from file");
+        System.out.println("0) Exit");
+        System.out.print("Choose: ");
+    }
 }
